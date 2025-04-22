312a1a04588f3ebf906ad525c5e613206c9f8b32
konbraphat51 <konbraphat@gmail.com>
2025-04-22 23:15:57 +0900

    data of Action

---
+++ Goap/Action.cs
@@ -0,0 +1,23 @@
+namespace TsunagiModule.Goap
+{
+    public struct Action
+    {
+        public string name { get; private set; }
+        public ConditionInterface condition { get; private set; }
+        public StateDiffSet stateDiffSet { get; private set; }
+        public float cost { get; private set; }
+
+        public Action(
+            string name,
+            ConditionInterface condition,
+            StateDiffSet stateDiffSet,
+            float cost
+        )
+        {
+            this.name = name;
+            this.condition = condition;
+            this.stateDiffSet = stateDiffSet;
+            this.cost = cost;
+        }
+    }
+}
