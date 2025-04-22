da6cab0ca2698768418ff5bfb47f0a1fe62093ca
konbraphat51 <konbraphat@gmail.com>
2025-04-22 23:30:38 +0900

    setting action

---
+++ Goap/GoapSolver.cs
@@ -0,0 +1,30 @@
+using System.Collections.Generic;
+
+namespace TsunagiModule.Goap
+{
+    public class GoapSolver
+    {
+        /// <summary>
+        /// Pool of actions
+        /// </summary>
+        /// <remarks>
+        /// action name -> Action
+        /// </remarks>
+        private Dictionary<string, Action> actionPool = new Dictionary<string, Action>();
+
+        public void AddAction(Action action)
+        {
+            actionPool.Add(action.name, action);
+        }
+
+        public void RemoveAction(string name)
+        {
+            actionPool.Remove(name);
+        }
+
+        public void ReplaceAction(string name, Action action)
+        {
+            actionPool[name] = action;
+        }
+    }
+}
