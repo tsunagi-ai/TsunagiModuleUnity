77b2508ffe32a1b41ccbfba5599682082362b5ac
konbraphat51 <konbraphat@gmail.com>
2025-04-23 00:56:28 +0900

    temp; rebase this

--- Goap/GoapSolver/GoapSolver_Pathfinding.cs
+++ Goap/GoapSolver/GoapSolver_Pathfinding.cs
@@ -1,12 +1,20 @@
 using System;
+using System.Collections.Generic;
 
 namespace TsunagiModule.Goap
 {
     public partial class GoapSolver
     {
+        /// <summary>
+        /// Cost estimation weights for each state index
+        /// </summary>
+        private Dictionary<string, float> stateIndexCostWeights = new Dictionary<string, float>();
+
         public Action[] Solve(State stateCurrent, Condition goal)
         {
             throw new NotImplementedException("Solve method not implemented yet.");
         }
+
+        private Dictionary<string, float> ComputeCostWeights() { }
     }
 }
