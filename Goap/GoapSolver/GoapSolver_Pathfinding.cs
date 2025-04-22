df03cc70f0e41447eec153c9c56cf511581b8d48
konbraphat51 <konbraphat@gmail.com>
2025-04-23 00:51:16 +0900

    dummy Pathfinding

---
+++ Goap/GoapSolver/GoapSolver_Pathfinding.cs
@@ -0,0 +1,12 @@
+using System;
+
+namespace TsunagiModule.Goap
+{
+    public partial class GoapSolver
+    {
+        public Action[] Solve(State stateCurrent, Condition goal)
+        {
+            throw new NotImplementedException("Solve method not implemented yet.");
+        }
+    }
+}
