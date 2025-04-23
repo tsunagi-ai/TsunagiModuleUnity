5db8ff3c47d2caf12f5982f31602df93e334c678
konbraphat51 <konbraphat@gmail.com>
2025-04-24 07:37:52 +0900

    refactor: implement ComputeCostWeights method for cost estimation

--- Goap/GoapSolver/GoapSolver_Pathfinding.cs
+++ Goap/GoapSolver/GoapSolver_Pathfinding.cs
@@ -12,9 +12,34 @@ namespace TsunagiModule.Goap
 
         public Action[] Solve(State stateCurrent, Condition goal)
         {
+            // compute cost weights
+            stateIndexCostWeights = ComputeCostWeights(stateCurrent);
+
             throw new NotImplementedException("Solve method not implemented yet.");
         }
 
-        private Dictionary<string, float> ComputeCostWeights() { }
+        private Dictionary<string, float> ComputeCostWeights(State stateCurrent)
+        {
+            // state index -> cost per diff
+            Dictionary<string, float> largestCostPerDiff = new Dictionary<string, float>();
+            foreach (string stateIndex in stateCurrent.indices)
+            {
+                largestCostPerDiff[stateIndex] = 0f;
+            }
+
+            foreach (Action action in actionPool.Values)
+            {
+                foreach (StateDiffInterface stateDiff in action.stateDiffSet.stateDiffes)
+                {
+                    // get cost per diff
+                    float costPerDiff = Math.Abs(action.cost / stateDiff.diff);
+                    if (largestCostPerDiff[stateDiff.stateIndex] < costPerDiff)
+                    {
+                        largestCostPerDiff[stateDiff.stateIndex] = costPerDiff;
+                    }
+                }
+            }
+            return largestCostPerDiff;
+        }
     }
 }
