9f8c49c1179edae6e5d123829a4329dd8d85601e
konbraphat51 <konbraphat@gmail.com>
2025-04-24 07:29:40 +0900

    implement StateDiffMapping struct for state value mapping operations

---
+++ Goap/StateDiff/StateDiffMapping.cs
@@ -0,0 +1,63 @@
+using System;
+using System.Collections.Generic;
+
+namespace TsunagiModule.Goap
+{
+    public struct StateDiffMapping<T> : StateDiffInterface
+        where T : struct, IEquatable<T>
+    {
+        public string stateIndex { get; private set; }
+        public Dictionary<T, T> mapping { get; set; }
+
+        public StateDiffMapping(string stateIndex, Dictionary<T, T> mapping)
+        {
+            this.stateIndex = stateIndex;
+            this.mapping = mapping;
+        }
+
+        public State Operate(State state, bool overwrite = true)
+        {
+            // cloning or not
+            State stateTarget;
+            if (overwrite)
+            {
+                stateTarget = state;
+            }
+            else
+            {
+                stateTarget = state.Clone();
+            }
+
+            GoapValueInterface targetValueInterface = stateTarget.GetValue(stateIndex);
+
+            // if both are same type...
+            if (targetValueInterface is GoapValue<T> targetValue)
+            {
+                // ...operate addtion
+
+                T currentValue = targetValue.value;
+
+                // if the current value is in the map...
+                if (mapping.TryGetValue(currentValue, out T value))
+                {
+                    // ...update the state with the mapped value
+                    stateTarget.SetNumericValue(stateIndex, value);
+                }
+                // if there isn't...
+                else
+                {
+                    // ...do nothing
+                }
+
+                return stateTarget;
+            }
+            else
+            {
+                // ...panic
+                throw new ArgumentException(
+                    "StateDiffMapping: Type mismatch in mapping operation and State value type."
+                );
+            }
+        }
+    }
+}
