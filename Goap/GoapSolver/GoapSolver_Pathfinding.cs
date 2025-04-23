ea24debf238f4db6db5068a37ef72a48f3790c81
konbraphat51 <konbraphat@gmail.com>
2025-04-24 07:46:34 +0900

    AstarQueue

--- Goap/GoapSolver/GoapSolver_Pathfinding.cs
+++ Goap/GoapSolver/GoapSolver_Pathfinding.cs
@@ -3,8 +3,40 @@ using System.Collections.Generic;
 
 namespace TsunagiModule.Goap
 {
+    //https://medium.com/@hanxuyang0826/mastering-pathfinding-with-a-star-a-practical-guide-and-c-implementation-f76f1643d8c3
     public partial class GoapSolver
     {
+        private class AstarQueue : IComparable<AstarQueue>
+        {
+            public State state;
+            public AstarQueue parent;
+            public float currentCost;
+            public float heuristicCost;
+            public float totalCost => currentCost + heuristicCost;
+
+            public AstarQueue(
+                State state,
+                AstarQueue parent,
+                float currentCost,
+                float heuristicCost
+            )
+            {
+                this.state = state;
+                this.parent = parent;
+                this.currentCost = currentCost;
+                this.heuristicCost = heuristicCost;
+            }
+
+            public int CompareTo(AstarQueue other)
+            {
+                if (totalCost < other.totalCost)
+                    return -1;
+                if (totalCost > other.totalCost)
+                    return 1;
+                return 0;
+            }
+        }
+
         /// <summary>
         /// Cost estimation weights for each state index
         /// </summary>
