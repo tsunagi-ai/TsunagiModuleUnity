2596438faae9d56c3649b37f92ca9d4c521cf249
konbraphat51 <konbraphat@gmail.com>
2025-04-25 12:57:20 +0900

    fix: check condition of the simulated state

--- Scripts/Goap/GoapSolver/GoapSolver_Pathfinding.cs
+++ Scripts/Goap/GoapSolver/GoapSolver_Pathfinding.cs
@@ -135,7 +135,7 @@ namespace TsunagiModule.Goap
                     foreach (GoapAction action in actionPool.Values)
                     {
                         // if the action is available...
-                        if (action.condition.IsSatisfied(stateCurrent))
+                        if (action.condition.IsSatisfied(current.state))
                         {
                             // ...go to this state
                             GoapState nextState = action.Simulate(current.state, overwrite: false);
